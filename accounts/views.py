b7c3676a48b89951e390f61ff472a9170053cbf5
josepontocruz <josericardo.developer@gmail.com>
2021-04-18 02:43:40 +0100

    feat:Add the authenticate backbone

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,41 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 from .models import *
-from .forms import OrderForm
+from .forms import OrderForm, CreateUserForm
+
+
+
+def registerPage(request):
+    form = CreateUserForm() #Authentication do django
+
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Registrado!!')
+            return redirect('login')
+
+    context = {'form': form}
+    return render(request, 'accounts/register.html', context)
+
+def loginPage(request):
+
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user) #loga o user...
+            return redirect('home') #django method auth
+
+
+    context = {}
+    return render(request, 'accounts/login.html', context)
 
 def home(request):
     orders = Order.objects.all()
